474844f27289bc65bec76d22287cd5b740a6c012
Alejandra Victoria <ale.victoria.alcaraz@gmail.com>
2012-02-29 19:15:15 -0600

    fix

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('main.views',
     url(r'^zombies/add/?$', 'create_zombie', name='add_zombie'),
     url(r'^zombies/(?P<pk>\d+)/edit/?$', 'update_zombie', name='update_zombie'),
     url(r'^zombies/(?P<pk>\d+)/delete/?$', 'delete_zombie', name='delete_zombie'),
-    url(r'^zombie/(?P<pk>\d+)/tweets/?$', 'zombie_tweets', name='zombie_tweets'),
+    #url(r'^zombie/(?P<pk>\d+)/tweets/?$', 'zombie_tweets', name='zombie_tweets'),
 )
